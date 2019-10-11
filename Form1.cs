1670e5545f8ba0aadaa6e2bfb0a917ee01f35e89
MartPMR <timpmr@gmail.com>
2019-10-12 00:14:17 +0300

    Day 2

--- Form1.cs
+++ Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -24,11 +26,50 @@ namespace Client
         private string _Message;
         public string Message { get { return _Message; }set {_Message = textBoxMessage.Text.ToString(); } }
 
-
+        TcpClient client;
+        StreamWriter sw;
+        StreamReader sr;
 
         public Form1()
         {
+            
             InitializeComponent();
+            IP = "192.168.0.102";
+            Port = 5050;
+            Nick = "Bob";
+
+            Task.Factory.StartNew(() =>
+            {
+                while(true)
+                {
+                    try
+                    {
+                        if (client?.Connect == true)
+                        {
+                            var line = sr.ReadLine();
+
+                            if(line !=null)
+                            {
+                                Chat += line + "\n";
+                            }
+                            else
+                            {
+                                client.Close();
+                                Chat += "Connected error" + "\n";
+                            }
+                        }
+                        Task.Delay(10).Wait();
+                    }
+                    catch(Exception)
+                    {
+
+                    }
+                }
+            });
+        }
+        public AsyncCommand ConnectCommand
+        {
+
         }
     }
 }
